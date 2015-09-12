f62d23f993754990d5f8d6ef69e997e2f235b4d3
Matt <27matt37@gmail.com>
2015-09-12 11:54:27 +0100

    fixes

--- db/migrate/20150907194206_create_reviews.rb
+++ db/migrate/20150907194206_create_reviews.rb
@@ -1,12 +1,11 @@
 class CreateReviews < ActiveRecord::Migration
-  def change
-      create_table "reviews", force: :cascade do |t|
-        t.string   "title"
-        t.string   "movie_id"
-        t.datetime "created_at", null: false
-        t.datetime "updated_at", null: false
-        t.text     "body"
-  end
+    def change
+        create_table "reviews", force: :cascade do |t|
+            t.string   "title"
+            t.string   "movie_id"
+            t.datetime "created_at", null: false
+            t.datetime "updated_at", null: false
+            t.text     "body"
+        end
     end
-  end
 end
