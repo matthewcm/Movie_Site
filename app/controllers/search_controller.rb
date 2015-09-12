fc4406f0fcc76b9fabf8967aae4acac1c228fd58
Matt <27matt37@gmail.com>
2015-09-12 11:16:39 +0100

    clean up

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,11 +4,7 @@ class SearchController < ApplicationController
   end
   def show
     @movie_service = MovieService.new
-    puts "============="
-    puts :q.sub('+','%'))
-    puts'bananapeach'
-    puts "============="
-    @movie_name = @movie_service.single_movie(:q)
+    @movie = @movie_service.single_movie(:q)
     redirect_to  "/search/{movie['imdbID']}"
   end
   private
