59919a31b4ec5098774285ac8d20c30980924567
Matt <27matt37@gmail.com>
2015-09-16 07:12:42 +0100

    precompiling

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,7 @@ module Matts_Movies
     console do
       ActiveRecord::Base.connection
     end
+    config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
