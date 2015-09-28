b6f0dfec13ade6a899040426b646a82167ba1f4e
Matt <27matt37@gmail.com>
2015-09-28 18:33:32 +0100

    Added admin control to delete reviews.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150923174920) do
+ActiveRecord::Schema.define(version: 20150928170542) do
 
   create_table "reviews", force: :cascade do |t|
     t.string   "title"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20150923174920) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.string   "password_digest"
+    t.integer  "level"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
